42e91163f55d6d8c51c544f1a76de766d971453f
Johan Ribb <johan.ribb@rocketsdots.com>
2020-03-05 13:52:51 +0100

    added pages :)

--- color-year/scripts.js
+++ color-year/scripts.js
@@ -1,4 +1,4 @@
-var components = ["base-styles", "header", "product-hero", "text", "static-slider", "video-hero", "color-list", "color-navigation"];
+var components = ["base-styles", "header", "product-hero", "text", "static-slider", "video-hero", "color-list", "color-navigation", "image-text", "video-reveal", "product-scroll-list"];
 
 
 $.each(components, function (index, value) {
