7de08ba79d1fd39bfc642fa54dd38cf49a69c7e7
Johan Ribb <johan.ribb@rocketsdots.com>
2020-03-11 16:50:25 +0100

    Added image etc

--- color-year/scripts.js
+++ color-year/scripts.js
@@ -1,28 +1,53 @@
 var components = ["base-styles", "header", "product-hero", "product-description", "static-slider", "video-hero", "color-list", "color-navigation", "image-text", "video-reveal", "product-scroll-list", "color-swoosh", "product-contact"];
 
-
 $.each(components, function (index, value) {
-    $.get("components/" + value + ".html", function (data) {
-        $.each($("component[data-name='" + value + "']"), function (index, existing) {
-            var json = $(existing).attr('data-parameters');
-            if (json) {
-                var parameterized = data;
-                json = JSON.parse(json);
+    $.get("components/" + value + ".html", function (template) {
+        $.each($("component[data-name='" + value + "']"), function (index, placeholder) {
+
+            var parameters = $(placeholder).find('data').html();
+
+            if (parameters) {
+
+                var json = JSON.parse(parameters);
+
                 Object.keys(json).forEach(function (k) {
-                    parameterized = parameterized.replace("{" + k + "}", json[k]);
+                    if (json[k] instanceof Array) {
+                        var sections = template.split(new RegExp("<" + k + ">|<\/" + k + ">"));
+                        if (sections.length == 3) {
+                            var result = "";
+
+                            for (var i = 0; i < json[k].length; i++) {
+
+                                var arrayTemplate = sections[1];
+
+                                Object.keys(json[k][i]).forEach(function (arrayKey) {
+                                    arrayTemplate = arrayTemplate.replace(new RegExp("{" + arrayKey + "}", "g"), json[k][i][arrayKey]);
+                                });
+                                result += arrayTemplate;
+                            }
+
+                            template = sections[0] + result + sections[2];
+                        }
+
+                    }
+                    else {
+                        template = template.replace(new RegExp('{' + k + '}', 'g'), json[k]);
+                    }
                 });
-                $(existing).replaceWith(parameterized)
-            } else {
-                $(existing).replaceWith(data);
             }
+            $(placeholder).replaceWith(template);
         });
 
         if (index == components.length - 1) {
-            setupComponents();
+
+            $('.c-color-navigation__link').each(function (index, value) {
+                $(value).attr('href', getPageUrl($(value).attr('data-name')));
+            });
         }
     });
 });
 
 
-function setupComponents() {
+function getPageUrl(name) {
+    return "page-details.html?color=" + name;
 }
