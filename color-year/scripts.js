d06bdb6a6181612e0d62c4ee9f9e652617823df5
Johan Ribb <johan.ribb@rocketsdots.com>
2020-03-18 15:09:09 +0100

    new copy

--- color-year/scripts.js
+++ color-year/scripts.js
@@ -16,21 +16,18 @@ var navigationData = {
         { "title": "Frö", "label": "Ljusbrun", "url": getPageUrl("frö"), "image": assetsPath + "color-nav-10.png" }
     ]
 };
-
 var contactUsData = {
     "header": "Prata färg med våra köksexperter!", "preamble": "Boka ett möte med en av våra köksdesigners för att snacka planering, material och färg av våra kök. Vi träffas digitalt, på telefon eller i butik", "link": "Boka ett möte", "url": "#"
 };
 var pageStartData = { "header": "Jordnära", "preamble": "Vår nya vattenbaserade färgkollektion", "button": "Upplev kollektionen", "url": "page-list.html", "image": imagePath + "marbodal-koks-farger.jpg" };
 var pageListData = {
-    "video-hero": { "video-url": videoPath + "jordnara.mp4", "header": "10 färger inspirerade av vår Nordiska natur", "preamble": "Vi kallar färgkollektionen Jordnära", "link": "Se alla våra färger" },
+    "video-hero": { "video-url": videoPath + "jordnara.mp4", "header": "Jordnära", "preamble": "10 färger inspirerade av vår Nordiska natur", "link": "Se alla våra färger" },
     "description": { "header": "Från de grönaste skogarna till de blåaste haven. Från de granitgrå bergen till de vidsträckta sandstränderna.", "preamble": "Från vintermorgonens frost till sommaråskans mullrande framfart. För att finna det vi sökte letade vi där allt en gång började. I vår natur. Här fann vi vår nya färgkollektion. Vi kallar den Jordnära.", "header-color": "#C3A472" },
     "sections": {
         "colors": [
             {
                 "header": "Tall",
                 "label": "Vår mörkgröna färg",
-                "preamble": "Känslan av att kliva ut i en skog är oslagbar.",
-                "preamble-mobile": "NCS S 5005-G50Y",
                 "image": imagePath + "list-tall.jpg",
                 "image-mobile": imagePath + "list-tall-mobile.jpg",
                 "cta": "Upplev tall",
@@ -41,8 +38,6 @@ var pageListData = {
             {
                 "header": "Sand",
                 "label": "Vår beiga färg",
-                "preamble": "Vår färg Sand är inspirerad av våra sandstränder som vi hittar längs vår vackra kust.",
-                "preamble-mobile": "NCS S 2005-Y20R",
                 "image": imagePath + "list-sand.jpg",
                 "image-mobile": imagePath + "list-sand-mobile.jpg",
                 "cta": "Upplev sand",
@@ -53,8 +48,6 @@ var pageListData = {
             {
                 "header": "Bark",
                 "label": "Vår bruna färg",
-                "preamble": "Alla har vi nog strosat genom en vacker höstskog. Inspirerade av skogens skiftande höstfärger skapade vi färgen Bark.",
-                "preamble-mobile": "NCS S 7005-Y20R",
                 "image": imagePath + "list-bark.jpg",
                 "image-mobile": imagePath + "list-bark-mobile.jpg",
                 "cta": "Upplev bark",
@@ -65,8 +58,6 @@ var pageListData = {
             {
                 "header": "Hav",
                 "label": "Vår blåa färg",
-                "preamble": "Likt havets vågor som slår mot en klippa, eller som vandrar upp för en strand, skiftar vår blåa färg Hav i ton och känsla beroende på ljus och övriga färger i rummet.",
-                "preamble-mobile": "NCS S 8005-R80B",
                 "image": imagePath + "list-hav.jpg",
                 "image-mobile": imagePath + "list-hav-mobile.jpg",
                 "cta": "Upplev hav",
@@ -77,8 +68,6 @@ var pageListData = {
             {
                 "header": "Frö",
                 "label": "Vår grå-beiga färg",
-                "preamble": "Moder jord är underbar. Från ett litet frö kan vi få de mest fantastiska råvarorna.",
-                "preamble-mobile": "NCS S4502-Y",
                 "image": imagePath + "list-fro.jpg",
                 "image-mobile": imagePath + "list-fro-mobile.jpg",
                 "cta": "Upplev frö",
@@ -89,8 +78,6 @@ var pageListData = {
             {
                 "header": "Granit",
                 "label": "Vår mörkgråa färg",
-                "preamble": "Stora delar av Sveriges berggrund består av granit.  Denna hårdsten kommer i många skiftande nyanser.",
-                "preamble-mobile": "NCS S 7500-N",
                 "image": imagePath + "list-granit.jpg",
                 "image-mobile": imagePath + "list-granit-mobile.jpg",
                 "cta": "Upplev granit",
@@ -101,8 +88,6 @@ var pageListData = {
             {
                 "header": "Åska",
                 "label": "Vår mellangråa färg",
-                "preamble": "När sommaråskan rullar in över våra åkermarker får himlen ett nästan onaturligt ljus.",
-                "preamble-mobile": "NCS S 5500-N",
                 "image": imagePath + "list-aska.jpg",
                 "image-mobile": imagePath + "list-aska-mobile.jpg",
                 "cta": "Upplev åska",
@@ -113,8 +98,6 @@ var pageListData = {
             {
                 "header": "Moln",
                 "label": "Vår ljusgråa färg",
-                "preamble": "De vackra lätta molnen som ligger högt upp i atmosfären under sommarmånaderna inspirerade oss till färgen Moln.",
-                "preamble-mobile": "NCS S 2500-N",
                 "image": imagePath + "list-moln.jpg",
                 "image-mobile": imagePath + "list-moln-mobile.jpg",
                 "cta": "Upplev moln",
@@ -125,8 +108,6 @@ var pageListData = {
             {
                 "header": "Lin",
                 "label": "Vår ljusgröna färg",
-                "preamble": "Från en vacker sommaräng hämtade vi vår inspiration till färgen Lin. En tidlös klassiker.",
-                "preamble-mobile": "NCS S 2005-G10Y",
                 "image": imagePath + "list-lin.jpg",
                 "image-mobile": imagePath + "list-lin-mobile.jpg",
                 "cta": "Upplev lin",
@@ -137,8 +118,6 @@ var pageListData = {
             {
                 "header": "Frost",
                 "label": "Vår vita färg",
-                "preamble": "Det första tecknet på att vintern är i antågande är det vackra vita skalet av glimrande frost som täcker buskar och grenar.",
-                "preamble-mobile": "NCS S 7005-Y20R",
                 "image": imagePath + "list-frost.jpg",
                 "image-mobile": imagePath + "list-frost-mobile.jpg",
                 "cta": "Upplev frost",
@@ -169,27 +148,26 @@ var pageDetailsData = {
             "header-color": "#989A85"
         },
         "video-reveal": {
-            "header": "Bjud in naturen",
+            "header": "Tall",
             "video": videoPath + "tall.mp4",
             "image-count": "20",
             "image-name": imagePath + "animations/tall_000"
         },
         "image-text": {
-            "header": "Fagerö Tall är senaste tillskottet i familjen gröna kök",
-            "preamble": "Fagerö är ett av våra mest uppskattade kök. Köket gör sig fint i en sekelskiftesvåning men kan stylas i både klassisk och lantlig stil.",
+            "header": "Våra gröna kök",
+            "preamble": "Tall är en grön nyans som håller över tid och som går att kombinera med olika material på bänkskivor och tillbehör.",
             "image": imagePath + "tall-article.jpg",
             "links": [{ "name": "Se alla gröna kök ›", "url": "#" }],
             "facts": "S8005 - R80B<br>Vattenbaserad",
             "swoosh-image": imagePath + "tall-swoosh.jpg"
         },
-
         "product-scroll-list": {
             "header": "Våra luckor i tall",
             "products": [
-                { "name": "Arkitekt Plus Tall", "description": "Vitmålad ramlucka av MDF. Vitrinlucka med klart eller frostat glas alt krysspröjs.", "url": "#", "padded-image": "true", "image": imagePath + "tall-scroll-1.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "tall-scroll-2.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "tall-scroll-3.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "tall-scroll-4.jpg" }
+                { "name": "Arkitekt Plus Tall", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "tall-scroll-1.jpg" },
+                { "name": "Lindö Tall", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "tall-scroll-2.jpg" },
+                { "name": "Form Tall", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "tall-scroll-3.jpg" },
+                { "name": "Fagerö tall", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "tall-scroll-4.jpg" }
             ]
         }
     },
@@ -203,44 +181,43 @@ var pageDetailsData = {
             "image": imagePath + "frost-hero.jpg",
             "image-mobile": imagePath + "frost-hero-mobile.jpg",
             "header-key": "Frost", "header":
-                "Våra ljusa kök i färgen",
-            "preamble": "NCS S8005 - R80B &middot; Vattenbaserad"
+                "Våra vita kök i färgen",
+            "preamble": "NCS S 0500-N &middot; Vattenbaserad"
         },
         "product-description": {
-            "header": "Känslan av att kliva ut i en skog är oslagbar.",
-            "preamble": "Luften fyller lungorna med ny kraft. Det stilla suset av vajande trädtoppar. Skogens dofter och ljud. Vår färg Tall är noga balanserad och utvald för att förmedla den känslan.",
+            "header": "Det första tecknet på att vintern är i antågande är det vackra vita skalet av glimrande frost som täcker buskar och grenar",
+            "preamble": " Den här kristallklara och rena känslan inspirerade oss till vår färg Frost. Det är en ljus och energirik färg som får våra flöden att gå snabbare. Den hjälper rummet att upplevas större och drar mot en modern och ren känsla.",
             "header-color": "#a7a7a7"
         },
         "video-reveal": {
-            "header": "Bjud in naturen",
+            "header": "Frost",
             "video": videoPath + "frost.mp4",
             "image-count": "20",
             "image-name": imagePath + "animations/frost_000"
         },
         "image-text": {
-            "header": "Lindö Tall är senaste tillskottet i familjen gröna kök",
-            "preamble": "Lindö är en klassisk exklusiv spegellucka med brett färgval och många vitrinval. Av alla våra kök är Lindö det som har mest profilerad ram och spegel.",
+            "header": "Frost",
+            "preamble": "Är en ljus och energirik färg som får våra flöden att gå snabbare. Den hjälper rummet att upplevas större och drar mot en modern och ren känsla.  Se Frost som en tom målarduk som du kan välja att forma  åt vilket håll du vill. Matcha med vitt för en minimalistisk känsla",
             "image": imagePath + "frost-article.jpg",
-            "links": [{ "name": "Se alla gröna kök ›", "url": "#" }],
-            "facts": "S8005 - R80B<br>Vattenbaserad",
+            "links": [{ "name": "Se alla vita kök ›", "url": "#" }],
+            "facts": "S 0500-N<br>Vattenbaserad",
             "swoosh-image": imagePath + "frost-swoosh.jpg"
         },
-
         "product-scroll-list": {
             "header": "Våra luckor i tall",
             "products": [
-                { "name": "Arkitekt Plus Tall", "description": "Vitmålad ramlucka av MDF. Vitrinlucka med klart eller frostat glas alt krysspröjs.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-1.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-2.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-3.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-4.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-5.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-6.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-7.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-8.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-9.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-10.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-11.jpg" },
-                { "name": "Lindö tall", "description": "Lucka med klart eller frostat glas alt krysspröjs. Vitmålad ramlucka av MDF Vitrin.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-12.jpg" }
+                { "name": "Arkitekt Plus Frost", "description": "Vitmålad ramlucka av MDF. Vitrinlucka med klart eller frostat glas alt krysspröjs.", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-1.jpg" },
+                { "name": "Öland Frost", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-2.jpg" },
+                { "name": "Aspekt Frost", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-3.jpg" },
+                { "name": "Torö Frost", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-4.jpg" },
+                { "name": "Viken Frost", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-5.jpg" },
+                { "name": "Vollo Frost", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-6.jpg" },
+                { "name": "Lindö Frost", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-7.jpg" },
+                { "name": "Form Frost", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-8.jpg" },
+                { "name": "Fagerö Frost", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-9.jpg" },
+                { "name": "Ekerö tall", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-10.jpg" },
+                { "name": "Vinga tall", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-11.jpg" },
+                { "name": "Arkitekt tall", "description": "", "url": "#", "padded-image": "true", "image": imagePath + "frost-scroll-12.jpg" }
             ]
         }
     }
